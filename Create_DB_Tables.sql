1facd0fb6cebca06c2a91b474f6f0e6d8bf38b16
chilinh428 <165115957+chilinh428@users.noreply.github.com>
2024-03-31 15:40:29 +0700

    Update

--- Create_DB_Tables.sql
+++ Create_DB_Tables.sql
@@ -3,7 +3,7 @@ CREATE DATABASE famous_painting;
 USE famous_painting;
 
 CREATE TABLE artist (
-	artist_id SMALLINT NOT NULL,
+    artist_id SMALLINT NOT NULL,
     full_name VARCHAR(30) NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     middle_names VARCHAR(30),
@@ -15,14 +15,14 @@ CREATE TABLE artist (
 );
 
 CREATE TABLE canvas_size (
-	size_id SMALLINT NOT NULL,
+    size_id SMALLINT NOT NULL,
     width TINYINT,
     height TINYINT,
     label VARCHAR(30)
 );
 
 CREATE TABLE museum (
-	museum_id SMALLINT NOT NULL,
+    museum_id SMALLINT NOT NULL,
     name VARCHAR(50) NOT NULL,
     address VARCHAR(50) NOT NULL,
     city VARCHAR(30) NOT NULL,
@@ -32,26 +32,26 @@ CREATE TABLE museum (
 );
 
 CREATE TABLE museum_hour (
-	museum_id SMALLINT NOT NULL,
+    museum_id SMALLINT NOT NULL,
     day VARCHAR(30) NOT NULL,
     open VARCHAR(30) NOT NULL,
     close VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE product_size (
-	work_id MEDIUMINT NOT NULL,
+    work_id MEDIUMINT NOT NULL,
     size_id SMALLINT NOT NULL,
     sale_price SMALLINT NOT NULL,
     regular_price SMALLINT NOT NULL
 );
 
 CREATE TABLE subject (
-	work_id MEDIUMINT NOT NULL,
+    work_id MEDIUMINT NOT NULL,
     subject VARCHAR(30)
 );
 
 CREATE TABLE work (
-	work_id MEDIUMINT NOT NULL,
+    work_id MEDIUMINT NOT NULL,
     name VARCHAR(100) NOT NULL,
     artist_id SMALLINT NOT NULL,
     style VARCHAR(30) NOT NULL,
